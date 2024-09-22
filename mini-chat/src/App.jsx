6a6c864917944534f08b46215ebac23c35bcc148
Krzysztof Poziomek <k.poziomek@selleo.com>
2024-09-22 21:30:42 +0200

    add web-mini-chat

---
+++ mini-chat/src/App.jsx
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Chat from "./Chat/Chat.jsx";
+
+function App() {
+
+  return (
+
+    <>
+      <Chat />
+    </>
+  )
+}
+
+export default App
